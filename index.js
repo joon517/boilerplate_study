cd4d3b3be05f72a228aa13f033bc1354924b1b3d
박중현 <joonghyun@bagjunghyeon-ui-MacBookPro.local>
2021-12-20 20:07:54 +0900

    lec7 complete

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser');
+const { User } = require("./models/Users");
 
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://joon517:admin@youtubeprac.wt9pe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -10,5 +18,21 @@ mongoose.connect('mongodb+srv://joon517:admin@youtubeprac.wt9pe.mongodb.net/myFi
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
