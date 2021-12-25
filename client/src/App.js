439efeb15e1597ebce88ef4cc22243168c52292b
joon <jh517.park@gmail.com>
2021-12-25 14:34:36 +0900

    lec20 complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+        </Routes>
+        </div>
+    </Router>
+  );
 }
 
 export default App;
